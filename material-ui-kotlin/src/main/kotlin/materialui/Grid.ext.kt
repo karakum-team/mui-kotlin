8ecfaf45bc14e55ee81716d26595c159c0c39a25
Victor Turansky <victor.turansky@gmail.com>
2021-07-24 03:57:43 +0300

    Update declarations

--- material-ui-kotlin/src/main/kotlin/materialui/Grid.ext.kt
+++ material-ui-kotlin/src/main/kotlin/materialui/Grid.ext.kt
@@ -43,6 +43,25 @@ external enum class GridDirection {
 
 @Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
 // language=JavaScript
+@JsName("""({s0: 0, s1: 1, s2: 2, s3: 3, s4: 4, s5: 5, s6: 6, s7: 7, s8: 8, s9: 9, s10: 10})""")
+external enum class GridSpacing {
+    s0,
+    s1,
+    s2,
+    s3,
+    s4,
+    s5,
+    s6,
+    s7,
+    s8,
+    s9,
+    s10,
+
+    ;
+}
+
+@Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
+// language=JavaScript
 @JsName("""({FLEX_START: 'flex-start', CENTER: 'center', FLEX_END: 'flex-end', SPACE_BETWEEN: 'space-between', SPACE_AROUND: 'space-around', SPACE_EVENLY: 'space-evenly'})""")
 external enum class GridJustification {
     FLEX_START,
@@ -62,6 +81,27 @@ external enum class GridWrap {
     NOWRAP,
     WRAP,
     WRAP_REVERSE,
+
+    ;
+}
+
+@Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
+// language=JavaScript
+@JsName("""({auto: 'auto', s1: 1, s2: 2, s3: 3, s4: 4, s5: 5, s6: 6, s7: 7, s8: 8, s9: 9, s10: 10, s11: 11, s12: 12})""")
+external enum class GridSize {
+    auto,
+    s1,
+    s2,
+    s3,
+    s4,
+    s5,
+    s6,
+    s7,
+    s8,
+    s9,
+    s10,
+    s11,
+    s12,
 
     ;
 }
