722cef70a4f70fc85995b55190cd7503ad05d459
Victor Turansky <victor.turansky@gmail.com>
2021-07-26 04:59:13 +0300

    Update declarations

--- material-ui-kotlin/src/main/kotlin/material/ClickAwayListener.kt
+++ material-ui-kotlin/src/main/kotlin/material/ClickAwayListener.kt
@@ -10,8 +10,28 @@ external interface ClickAwayListenerProps : react.RProps {
      * The wrapped element.
      */
     var children: dynamic
-    var disableReactTree: dynamic
+
+    /**
+     * If `true`, the React tree is ignored and only the DOM tree is considered.
+     * This prop changes how portaled elements are handled.
+     * @default false
+     */
+    var disableReactTree: Boolean
+
+    /**
+     * The mouse event to listen to. You can disable the listener by providing `false`.
+     * @default 'onClick'
+     */
     var mouseEvent: dynamic
+
+    /**
+     * Callback fired when a "click away" event is detected.
+     */
     var onClickAway: dynamic
+
+    /**
+     * The touch event to listen to. You can disable the listener by providing `false`.
+     * @default 'onTouchEnd'
+     */
     var touchEvent: dynamic
 }
