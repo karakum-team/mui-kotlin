8bcc4df1978783cd48373cd4e4653d8d62e20b2a
Victor Turansky <victor.turansky@gmail.com>
2021-08-06 03:09:34 +0300

    Wrappers `pre.226`

--- material-ui-kotlin/build.gradle.kts
+++ material-ui-kotlin/build.gradle.kts
@@ -3,9 +3,12 @@ plugins {
     `material-ui-declarations`
 }
 
+val kotlinWrappersVersion = "0.0.1-pre.226-kotlin-1.5.21"
+
 dependencies {
     val materialVersion = property("material-ui.version") as String
     implementation(npm("@material-ui/core", materialVersion))
 
-    implementation("org.jetbrains.kotlin-wrappers:kotlin-react:17.0.2-pre.224-kotlin-1.5.21")
+    implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:${kotlinWrappersVersion}"))
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react")
 }
