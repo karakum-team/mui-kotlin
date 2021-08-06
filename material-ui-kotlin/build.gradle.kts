065e9d23945bfe62437a0cf5f0d11846fe9c3cfa
Victor Turansky <victor.turansky@gmail.com>
2021-08-06 03:21:43 +0300

    Versions in one section

--- material-ui-kotlin/build.gradle.kts
+++ material-ui-kotlin/build.gradle.kts
@@ -3,10 +3,10 @@ plugins {
     `material-ui-declarations`
 }
 
+val materialVersion = property("material-ui.version") as String
 val kotlinWrappersVersion = "0.0.1-pre.226-kotlin-1.5.21"
 
 dependencies {
-    val materialVersion = property("material-ui.version") as String
     implementation(npm("@material-ui/core", materialVersion))
 
     implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:${kotlinWrappersVersion}"))
