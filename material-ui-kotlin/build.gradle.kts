ccf855408f43d4a24b84edf840a0677737b33fa8
Victor Turansky <victor.turansky@gmail.com>
2021-08-27 04:19:36 +0300

    Wrappers `pre.238`

--- material-ui-kotlin/build.gradle.kts
+++ material-ui-kotlin/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 val materialVersion = property("material-ui.version") as String
-val kotlinWrappersVersion = "0.0.1-pre.237-kotlin-1.5.30"
+val kotlinWrappersVersion = "0.0.1-pre.238-kotlin-1.5.30"
 
 dependencies {
     implementation(npm("@material-ui/core", materialVersion))
