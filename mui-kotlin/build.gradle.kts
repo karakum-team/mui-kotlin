d45f8a4ae269e2f5ab3cc8eb462f67e69441af02
Victor Turansky <victor.turansky@gmail.com>
2021-09-14 07:03:30 +0300

    Update properties

--- mui-kotlin/build.gradle.kts
+++ mui-kotlin/build.gradle.kts
@@ -3,8 +3,8 @@ plugins {
     `material-declarations`
 }
 
-val materialVersion = property("material.version") as String
-val kotlinWrappersVersion = "0.0.1-pre.243-kotlin-1.5.30"
+val materialVersion = property("mui.version") as String
+val kotlinWrappersVersion = "0.0.1-pre.244-kotlin-1.5.30"
 
 dependencies {
     implementation(npm("@mui/material", materialVersion))
