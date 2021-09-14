cff1d041c977adaa45006bd9836a274d9569171b
Victor Turansky <victor.turansky@gmail.com>
2021-09-14 07:23:45 +0300

    Update versions

--- mui-kotlin/build.gradle.kts
+++ mui-kotlin/build.gradle.kts
@@ -3,12 +3,12 @@ plugins {
     `mui-declarations`
 }
 
-val materialVersion = property("mui.version") as String
-val kotlinWrappersVersion = "0.0.1-pre.244-kotlin-1.5.30"
+val muiVersion = property("mui.version") as String
+val kotlinWrappersVersion = property("kotlin-wrappers.version") as String
 
 dependencies {
-    implementation(npm("@mui/material", materialVersion))
+    implementation(npm("@mui/material", muiVersion))
 
-    implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:${kotlinWrappersVersion}"))
+    implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:$kotlinWrappersVersion"))
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom")
 }
