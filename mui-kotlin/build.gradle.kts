73ec7959c239fe3898fb04516dc87be8b0d19d0f
Victor Turansky <victor.turansky@gmail.com>
2021-10-05 01:15:23 +0300

    Add `@mui/lab` dependency

--- mui-kotlin/build.gradle.kts
+++ mui-kotlin/build.gradle.kts
@@ -4,10 +4,12 @@ plugins {
 }
 
 val muiVersion = property("mui.version") as String
+val muiLabVersion = property("mui-lab.version") as String
 val kotlinWrappersVersion = property("kotlin-wrappers.version") as String
 
 dependencies {
     implementation(npm("@mui/material", muiVersion))
+    implementation(npm("@mui/lab", muiLabVersion))
 
     implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:$kotlinWrappersVersion"))
     implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom")
