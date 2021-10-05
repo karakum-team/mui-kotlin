a23ee602421a020728c3c4c691ae053beadd9842
Victor Turansky <victor.turansky@gmail.com>
2021-10-06 01:45:13 +0300

    Update declarations

---
+++ mui-kotlin/src/main/kotlin/mui/lab/CalendarPickerSkeleton.kt
@@ -0,0 +1,18 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("@mui/lab/CalendarPickerSkeleton")
+@file:JsNonModule
+
+package mui.material
+
+external interface CalendarPickerSkeletonProps : react.Props {
+    /**
+     * Override or extend the styles applied to the component.
+     */
+    var classes: CalendarPickerSkeletonClasses
+
+    /**
+     * The system prop that allows defining system overrides as well as additional CSS styles.
+     */
+    var sx: SxProps<Theme>
+}
---
+++ mui-kotlin/src/main/kotlin/mui/lab/DateRangePickerDay.kt
@@ -0,0 +1,48 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("@mui/lab/DateRangePickerDay")
+@file:JsNonModule
+
+package mui.material
+
+external interface DateRangePickerDayProps : react.Props {
+    /**
+     * Set to `true` if the `day` is in a highlighted date range.
+     */
+    var isHighlighting: Boolean
+
+    /**
+     * Set to `true` if the `day` is the end of a highlighted date range.
+     */
+    var isEndOfHighlighting: Boolean
+
+    /**
+     * Set to `true` if the `day` is the start of a highlighted date range.
+     */
+    var isStartOfHighlighting: Boolean
+
+    /**
+     * Set to `true` if the `day` is in a preview date range.
+     */
+    var isPreviewing: Boolean
+
+    /**
+     * Set to `true` if the `day` is the start of a highlighted date range.
+     */
+    var isEndOfPreviewing: Boolean
+
+    /**
+     * Set to `true` if the `day` is the end of a highlighted date range.
+     */
+    var isStartOfPreviewing: Boolean
+
+    /**
+     * Override or extend the styles applied to the component.
+     */
+    var classes: DateRangePickerDayClasses
+
+    /**
+     * The system prop that allows defining system overrides as well as additional CSS styles.
+     */
+    var sx: SxProps<Theme>
+}
