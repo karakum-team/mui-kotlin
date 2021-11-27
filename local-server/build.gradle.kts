53de4c8ccf183b2d4e62e59840cebfb912710801
Victor Turansky <victor.turansky@gmail.com>
2021-11-27 21:51:27 +0300

    Use icons on local server

--- local-server/build.gradle.kts
+++ local-server/build.gradle.kts
@@ -8,6 +8,7 @@ devServer {
 
 dependencies {
     implementation(project(":mui-kotlin"))
+    implementation(project(":mui-icons-kotlin"))
 
     implementation(npm("@emotion/react", "11.6.0"))
     implementation(npm("@emotion/styled", "11.6.0"))
--- local-server/src/main/kotlin/App.kt
+++ local-server/src/main/kotlin/App.kt
@@ -1,4 +1,6 @@
 import kotlinx.browser.document
+import mui.icons.material.Abc
+import mui.icons.material.SouthEast
 import mui.material.SvgIcon
 import react.dom.render
 import react.dom.svg.ReactSVG.path
@@ -15,5 +17,8 @@ fun main() {
                 attrs.d = "M9 16.17 4.83 12l-1.42 1.41L9 19 21 7l-1.41-1.41z"
             }
         }
+
+        Abc()
+        SouthEast()
     }
 }
