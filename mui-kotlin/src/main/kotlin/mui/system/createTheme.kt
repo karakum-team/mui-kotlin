a3c7686a0cf71b3252954b42ef6a260d796e0f54
Victor Turansky <victor.turansky@gmail.com>
2021-12-06 01:44:58 +0300

    Update declarations

---
+++ mui-kotlin/src/main/kotlin/mui/system/createTheme.kt
@@ -0,0 +1,54 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("@mui/system/createTheme/createTheme")
+@file:JsNonModule
+
+package mui.system
+
+external interface ThemeOptions {
+    var shape: dynamic
+
+    var breakpoints: dynamic
+
+    var direction: Direction?
+
+    var mixins: dynamic
+
+    var palette: dynamic
+
+    var shadows: dynamic
+
+    var spacing: dynamic
+
+    var transitions: dynamic
+
+    var components: dynamic
+
+    var typography: dynamic
+
+    var zIndex: dynamic
+}
+
+external interface Theme {
+    var shape: dynamic
+
+    var breakpoints: dynamic
+
+    var direction: Direction
+
+    var palette: dynamic
+
+    var shadows: dynamic
+
+    var spacing: dynamic
+
+    var transitions: dynamic
+
+    var components: dynamic
+
+    var mixins: dynamic
+
+    var typography: dynamic
+
+    var zIndex: dynamic
+}
