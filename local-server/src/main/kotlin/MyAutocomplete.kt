f854846782f032176e5a28e47a011c7f0e607d90
Victor Turansky <victor.turansky@gmail.com>
2021-12-08 03:23:15 +0300

    Update `Autocomplete` example

--- local-server/src/main/kotlin/MyAutocomplete.kt
+++ local-server/src/main/kotlin/MyAutocomplete.kt
@@ -1,31 +1,24 @@
-import kotlinext.js.Object
 import mui.material.Autocomplete
 import mui.material.AutocompleteProps
 import mui.material.TextField
+import react.FC
 import react.Props
 import react.ReactNode
-import react.buildElement
-import react.fc
+import react.create
 
-val MyAutocomplete = fc<Props>("MyAutocomplete") {
+val MyAutocomplete = FC<Props> {
+    @Suppress("UPPER_BOUND_VIOLATED")
     Autocomplete<AutocompleteProps<String>> {
-        attrs {
-            options = arrayOf("foo", "bar", "baz")
+        options = arrayOf("foo", "bar", "baz")
 
-            disablePortal = true
+        disablePortal = true
 
-            renderInput = { params ->
-                buildElement {
-                    TextField {
-                        attrs {
-                            // TODO: Use `unaryPlus` instead
-                            Object.assign(this, params)
+        renderInput = { params ->
+            TextField.create {
+                +params
 
-                            label = ReactNode("Choose value")
-                            variant = "outlined"
-                        }
-                    }
-                }
+                label = ReactNode("Choose value")
+                variant = "outlined"
             }
         }
     }
