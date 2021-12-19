301d860789ba90cf474f5e179b81a611227e3976
Victor Turansky <victor.turansky@gmail.com>
2021-12-20 01:29:17 +0300

    Wrappers `pre.282`

--- local-server/build.gradle.kts
+++ local-server/build.gradle.kts
@@ -6,10 +6,16 @@ devServer {
     root = "App"
 }
 
+val kotlinWrappersVersion = property("kotlin-wrappers.version") as String
+
 dependencies {
+    implementation(enforcedPlatform("org.jetbrains.kotlin-wrappers:kotlin-wrappers-bom:$kotlinWrappersVersion"))
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom")
+
     implementation(project(":mui-kotlin"))
     implementation(project(":mui-icons-kotlin"))
 
-    implementation(npm("@emotion/react", "11.6.0"))
+    implementation(npm("@emotion/react", "11.7.1"))
     implementation(npm("@emotion/styled", "11.6.0"))
 }
