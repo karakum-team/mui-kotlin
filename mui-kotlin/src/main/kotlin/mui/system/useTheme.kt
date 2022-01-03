456baa35415a938f95ba216d6731609721f5f300
Victor Turansky <victor.turansky@gmail.com>
2022-01-03 05:23:34 +0300

    Update declarations

---
+++ mui-kotlin/src/main/kotlin/mui/system/useTheme.kt
@@ -0,0 +1,9 @@
+// Automatically generated - do not modify!
+
+@file:JsModule("@mui/system/useTheme")
+@file:JsNonModule
+
+package mui.system
+
+@JsName("default")
+external fun <T : Theme> useTheme(defaultTheme: T? = definedExternally): T
