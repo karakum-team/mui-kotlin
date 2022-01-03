56d2e979c144f2808076490e092d4ebd59a3c7d7
Victor Turansky <victor.turansky@gmail.com>
2022-01-03 07:21:39 +0300

    Prepare `useMediaQuery` conversion

--- buildSrc/src/main/kotlin/com/github/turansky/mui/FunctionType.kt
+++ buildSrc/src/main/kotlin/com/github/turansky/mui/FunctionType.kt
@@ -8,6 +8,7 @@ internal fun String.toFunctionType(): String? {
         return null
 
     return replace(" => ", "->")
+        .replace("{\n    matches: boolean;\n}", DYNAMIC)
         .replace("MouseEvent | TouchEvent", "org.w3c.dom.events.UIEvent")
         .replace("React.SyntheticEvent | Event", "react.dom.events.SyntheticEvent<*, *>")
         .replace("React.SyntheticEvent<any> | Event", "react.dom.events.SyntheticEvent<*, *>")
