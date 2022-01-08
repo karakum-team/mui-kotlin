9a4a0312a2f6d65814a001c36f42d3cf63948cf6
Victor Turansky <victor.turansky@gmail.com>
2022-01-09 02:33:21 +0300

    Static import

--- local-server/src/main/kotlin/TableCell.kt
+++ local-server/src/main/kotlin/TableCell.kt
@@ -1,13 +1,14 @@
 import mui.material.TableCell
 import react.Fragment
 import react.create
-import react.dom.html.ReactHTML
+import react.dom.html.ReactHTML.td
+import react.dom.html.ReactHTML.th
 
 fun cell() {
     Fragment.create {
         TableCell {
-            component = ReactHTML.td
-            component = ReactHTML.th
+            component = td
+            component = th
         }
     }
 }
