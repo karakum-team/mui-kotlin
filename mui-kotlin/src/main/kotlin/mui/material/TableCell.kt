873348ea45b312505e97d05711f7f80461e4e8f1
Victor Turansky <victor.turansky@gmail.com>
2022-02-26 22:11:00 +0300

    Update declarations

--- mui-kotlin/src/main/kotlin/mui/material/TableCell.kt
+++ mui-kotlin/src/main/kotlin/mui/material/TableCell.kt
@@ -32,7 +32,7 @@ external interface TableCellProps :
      * The component used for the root node.
      * Either a string to use a HTML element or a component.
      */
-    var component: react.ElementType<TableCellBaseProps>?
+    var component: react.ElementType<*>?
 
     /**
      * Sets the padding applied to the cell.
