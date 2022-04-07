d709684b415facd50846f3eedd66d9bbc345c8ee
Victor Turansky <victor.turansky@gmail.com>
2022-04-08 02:23:57 +0400

    Update declarations

---
+++ mui-kotlin/src/main/kotlin/muix/pickers/YearPicker.classes.kt
@@ -0,0 +1,8 @@
+// Automatically generated - do not modify!
+
+package muix.pickers
+
+external interface YearPickerClasses {
+    /** Styles applied to the root element. */
+    var root?: ClassName
+}
---
+++ mui-kotlin/src/main/kotlin/muix/pickers/YearPicker.mui.kt
@@ -0,0 +1,16 @@
+// Automatically generated - do not modify!
+
+@file:Suppress(
+    "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
+)
+
+package muix.pickers
+
+@Suppress("NAME_CONTAINS_ILLEGAL_CHARS")
+// language=JavaScript
+@JsName("""(/*union*/{root?: 'MuiYearPicker-root?'}/*union*/)""")
+sealed external interface MuiYearPicker {
+    companion object {
+        val root?: ClassName
+    }
+}
