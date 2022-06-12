85cf0d1dd1d61081d27cab914c2335b1b2e05e68
Victor Turansky <victor.turansky@gmail.com>
2022-06-13 02:15:05 +0300

    Exclude hooks

--- buildSrc/src/main/kotlin/karakum/mui/FunctionConverter.kt
+++ buildSrc/src/main/kotlin/karakum/mui/FunctionConverter.kt
@@ -17,6 +17,13 @@ internal fun findDefaultFunction(
         "useMenuItem",
         "useSelect",
         "useAutocomplete",
+
+        "useSlider",
+        "useSwitch",
+        "useTab",
+        "useTabPanel",
+        "useTabsList",
+
         -> return null
     }
 
@@ -31,6 +38,7 @@ internal fun findDefaultFunction(
             .mapNotNull { findDefaultFunction(name, it) }
             .joinToString("\n\n")
 
+    println(name)
     val (before, source) = content.split("export default function ")
 
     val comment = if (before.endsWith("**/\n")) {
