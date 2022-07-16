d9d8a1a515358348ebc54c0e0cf63a12a2842d54
Victor Turansky <victor.turansky@gmail.com>
2022-07-16 17:39:00 +0300

    Fix `DateAdapter` signature

--- buildSrc/src/main/kotlin/karakum/mui/DateAdapter.kt
+++ buildSrc/src/main/kotlin/karakum/mui/DateAdapter.kt
@@ -1,7 +1,7 @@
 package karakum.mui
 
 internal const val DATE_ADAPTER = "DateAdapter"
-internal const val DATE_ADAPTER_BODY = "typealias $DATE_ADAPTER = dateio.core.IUtils<*>"
+internal const val DATE_ADAPTER_BODY = "typealias $DATE_ADAPTER = JsClass<dateio.core.IUtils<*>>"
 
 internal val DATE_ADAPTERS = listOf(
     "AdapterDateFns",
