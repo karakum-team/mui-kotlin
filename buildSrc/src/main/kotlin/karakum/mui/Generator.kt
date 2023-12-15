36f254e1863002e1eba753db50d484c5f4b4c428
porotkin <preload737773@gmail.com>
2023-12-15 08:39:09 +0400

    Generator: Material Size & Regenerate Declaration

--- buildSrc/src/main/kotlin/karakum/mui/Generator.kt
+++ buildSrc/src/main/kotlin/karakum/mui/Generator.kt
@@ -121,13 +121,26 @@ private val MATERIAL_SIZE = """
     "NAME_CONTAINS_ILLEGAL_CHARS",
     "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
 )
-// language=JavaScript
-@JsName("(/*union*/{small: 'small', medium: 'medium', normal: 'normal', large: 'large'}/*union*/)")
+@JsVirtual()
 sealed external interface Size {
-    object small : Size, BaseSize, NormalSize
-    object medium : Size, BaseSize
-    object normal : NormalSize
-    object large : Size
+    companion object {
+        @JsValue('small')
+        val small: Size
+
+        @JsValue('medium')
+        val pkcs8: Size
+
+        @JsValue("normal")
+        val normal: Size
+
+        @JsValue("large")
+        val large: Size
+    }
+
+    sealed interface small : Size, BaseSize, NormalSize
+    sealed interface medium : Size, BaseSize
+    sealed interface normal : NormalSize
+    sealed interface large : Size
 }
 
 sealed external interface BaseSize
--- mui-kotlin/src/jsMain/kotlin/mui/material/Size.kt
+++ mui-kotlin/src/jsMain/kotlin/mui/material/Size.kt
@@ -2,17 +2,33 @@
 
 package mui.material
 
+import seskar.js.JsValue
+import seskar.js.JsVirtual
+
 @Suppress(
     "NAME_CONTAINS_ILLEGAL_CHARS",
     "NESTED_CLASS_IN_EXTERNAL_INTERFACE",
 )
-// language=JavaScript
-@JsName("(/*union*/{small: 'small', medium: 'medium', normal: 'normal', large: 'large'}/*union*/)")
+@JsVirtual()
 sealed external interface Size {
-    object small : Size, BaseSize, NormalSize
-    object medium : Size, BaseSize
-    object normal : NormalSize
-    object large : Size
+    companion object {
+        @JsValue("small")
+        val small: Size
+
+        @JsValue("medium")
+        val pkcs8: Size
+
+        @JsValue("normal")
+        val normal: Size
+
+        @JsValue("large")
+        val large: Size
+    }
+
+    sealed interface small : Size, BaseSize, NormalSize
+    sealed interface medium : Size, BaseSize
+    sealed interface normal : NormalSize
+    sealed interface large : Size
 }
 
 sealed external interface BaseSize
