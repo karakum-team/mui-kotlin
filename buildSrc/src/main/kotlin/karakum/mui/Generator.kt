44752a0484c88a27c88829efb4123d5fd1215159
Alexander Popoff <alexander.popoff@flysto.net>
2024-05-02 16:13:44 +0300

    `Size.pkcs8` -> `Size.medium`

--- buildSrc/src/main/kotlin/karakum/mui/Generator.kt
+++ buildSrc/src/main/kotlin/karakum/mui/Generator.kt
@@ -133,7 +133,7 @@ sealed external interface Size {
         val small: small
 
         @JsValue("medium")
-        val pkcs8: medium
+        val medium: medium
 
         @JsValue("normal")
         val normal: normal
