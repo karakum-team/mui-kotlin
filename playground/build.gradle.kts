c9be4ae56903fd1f3e312a32e56c21e27a616b39
Alexander Popoff <alexander.popoff@flysto.net>
2024-05-02 12:33:23 +0300

    KFC `7.40.1` (with Seskar `2.41.0` and MUI patch with ES modules enabled)

--- playground/build.gradle.kts
+++ playground/build.gradle.kts
@@ -15,3 +15,7 @@ dependencies {
     jsMainImplementation(npm("@emotion/react", "11.9.0"))
     jsMainImplementation(npm("@emotion/styled", "11.8.1"))
 }
+
+tasks.patchWebpackConfig {
+    patch(file("mui-patch.js"))
+}
