78643ada747fabbc056525226e23f9065d574076
Alexander Popoff <alexander.popoff@flysto.net>
2024-12-25 22:36:44 +0200

    Remove react overrides

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,6 @@
-import org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootPlugin
-import org.jetbrains.kotlin.gradle.targets.js.npm.NpmExtension
-
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.jsPlainObjects) apply false
-}
-
-plugins.withType<NodeJsRootPlugin> {
-    the<NpmExtension>().apply {
-        override("react", "^19.0.0")
-        override("react-dom", "^19.0.0")
-    }
 }
 
 tasks.wrapper {
